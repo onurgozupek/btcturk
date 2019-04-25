8d6bd407afb55025eca0422058f1d5fb1c4194a8
onurgozupek <onurgozupek@users.noreply.github.com>
2019-04-25 15:41:27 +0300

    Update example.php
    Added order examples on all order methods and order types.

--- example.php
+++ example.php
@@ -5,6 +5,27 @@ include("src/Client.php");
 
 $key = 'PUBLIC_KEY';
 $secret = 'PRIVATE_KEY';
-$b = new Client ($key, $secret);
-var_dump ($b->getBalances());
+$request = new BtcTurk ($key, $secret);
+print_r($request->getBalances());
 echo "\n\n";
+// Market Sell Order for XMLTRY pair
+print_r($request->getMarketSell("XLMTRY","20","00", 4));
+
+// Market Buy Order for BTCTRY pair
+print_r($request->getMarketBuy("BTCTRY","0","01", 2));
+
+// Stop Buy Order for XLMTRY pair
+// Place 20.00XLM buy order at 0.6000TRY when last price is 0.6140
+print_r($request->getStopBuy("XLMTRY","20","00","0","6000","0","6140", 4));
+
+// Stop Sell Order for XLMTRY pair
+// Place 20.75XRP sell order at 1.80TRY when last price is 1.81
+print_r($request->getStopSell("XRPTRY","20","75","1","80","1","81", 2));
+
+// Limit Buy Order for XLMTRY pair
+// Place 20.00XLM buy order at 0.6000TRY
+print_r($request->getLimitBuy("XLMTRY","20","00","0","6000", 4));
+
+// Limit Sell Order for XLMTRY pair
+// Place 20.75XRP sell order at 1.80TRY
+print_r($request->getLimitSell("XRPTRY","20","75","1","80", 2));
